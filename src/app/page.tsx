9a2ca3b9180e631341531847e87838672e5d2007
soulo <nikossoul2000@gmail.com>
2025-11-21 22:47:31 +0200

    database & prisma client initialization

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,33 @@
-import Image from "next/image";
+import prisma from "@/lib/prisma";
+import { randomUUID } from "crypto";
 
 export default function Home() {
+  async function createUser(formData: FormData) {
+    "use server";
+
+    const name = formData.get("name") as string;
+    const email = formData.get("email") as string;
+
+    await prisma.user.create({
+      data: {
+        id: "1",
+        name,
+        email,
+      },
+    });
+  }
+
   return (
-    <div className="my-10 justify-center text-center text-5xl font-bold text-purple-500">
-      Hello world!
-    </div>
+    <main className="p-8">
+      <h1>Create User</h1>
+
+      <form action={createUser} className="mt-4 flex flex-col gap-4">
+        <input type="text" name="name" placeholder="Name..." className="border p-2" />
+        <input type="email" name="email" placeholder="Email..." className="border p-2" />
+        <button type="submit" className="bg-blue-500 p-2 text-white">
+          Create
+        </button>
+      </form>
+    </main>
   );
 }
