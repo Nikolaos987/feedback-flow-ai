9ee5680b750b2c5f39f674968ebb4cdb95c2ae6a
soulo <nikossoul2000@gmail.com>
2025-11-26 22:57:49 +0200

    google authentication implementation

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,11 +1,15 @@
 import { usernameClient } from "better-auth/client/plugins";
 import { createAuthClient } from "better-auth/react";
-export const authClient = createAuthClient({
-  /** The base URL of the server (optional if you're using the same domain) */
-  // baseURL: "http://localhost:3000",
-  baseURL: process.env.NEXT_PUBLIC_APP_URL,
-});
 
-export const { signIn, signUp, useSession } = createAuthClient({
+export const authClient = createAuthClient({
+  baseURL: process.env.NEXT_PUBLIC_APP_URL,
   plugins: [usernameClient()],
 });
+
+export const { signIn, signUp, useSession, signOut } = authClient;
+
+export const googleSignIn = async () => {
+  return await signIn.social({
+    provider: "google",
+  });
+};
