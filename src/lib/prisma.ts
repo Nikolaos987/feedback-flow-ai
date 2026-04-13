bae43503b0969f60106f806aaa883fbdb031705d
soulo <nikossoul2000@gmail.com>
2026-04-13 17:30:03 +0300

    deployment fixes

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -2,15 +2,23 @@
 
 import { PrismaClient } from "@/generated/prisma/client";
 
-let prisma: PrismaClient;
+// let prisma: PrismaClient;
 
-if (process.env.NODE_ENV === "production") {
-  prisma = new PrismaClient();
-} else {
-  if (!global.prisma) {
-    global.prisma = new PrismaClient();
-  }
-  prisma = global.prisma;
-}
+// if (process.env.NODE_ENV === "production") {
+//   prisma = new PrismaClient();
+// } else {
+//   if (!global.prisma) {
+//     global.prisma = new PrismaClient();
+//   }
+//   prisma = global.prisma;
+// }
+
+const globalForPrisma = global as unknown as {
+  prisma: PrismaClient;
+};
+
+const prisma = globalForPrisma.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
 export default prisma;
